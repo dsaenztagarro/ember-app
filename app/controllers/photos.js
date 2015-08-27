35664f69e7bebb04949ebeb1dd5ffe25f94016cd
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-08-27 14:57:28 +0200

    Added data model in tab

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  cacheService: Ember.inject.service('cache'),
   actions: {
     select(photo) {
+      this.get('cacheService').add('photo', photo);
       console.log(photo);
       this.transitionToRoute('photos.properties');
     }
---
+++ app/routes/photos/properties.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  /*
+  cacheService: Ember.inject.service('cache'),
+  model: function() {
+    return this.get('cacheService').get('photo');
+  }
+  */
+});
