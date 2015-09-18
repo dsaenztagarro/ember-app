a76c2a0fe18cbd83c24d507a459a6bd8b2726c8a
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-09-18 15:15:54 +0200

    Fix photo properties bindings

--- app/controllers/photo.js
+++ app/controllers/photo.js
@@ -1,17 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  /*
-  photosController: Ember.inject.controller('photos'),
-  photos: Ember.computed.reads('model.photosphotosController.model.photos'),
-  photo: Ember.computed.reads('photosController.model.photo'),
-  */
   photos: Ember.computed.reads('model.photos'),
   photo: Ember.computed.reads('model.photo'),
   actions: {
     selectImage(photo) {
       this.set('photo', photo);
-      this.transitionToRoute('photo.properties', photo.id);
     }
   }
 });
--- app/controllers/photo/properties.js
+++ app/controllers/photo/properties.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  photoController: Ember.inject.controller('photo'),
+  photo: Ember.computed.reads('photoController.photo'),
   actions: {
     selectImage(photo) {
       this.get('cacheService').add('photo', photo);
