1110fc1f9f6bd67b4137e1defc6aea9d8e70628a
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-09-24 17:20:00 +0200

    Extracted common services and components

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    console.log('application#route');
-  },
-
   actions: {
     selectTab(tab) {
       this.transitionTo(tab.routeName);
--- app/services/cache.js
+++ app/services/cache.js
@@ -1,12 +1,3 @@
-import Ember from 'ember';
+import CoreCliServiceCache from 'ember-services/services/cache';
 
-var data = {};
-
-export default Ember.Service.extend({
-  add: function(key, value) {
-    data[key] = value;
-  },
-  get: function(key) {
-    return data[key];
-  }
-});
+export default CoreCliServiceCache.extend();
