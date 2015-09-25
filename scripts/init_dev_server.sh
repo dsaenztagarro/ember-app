44d05b3a17d233fbe42b1e6292352592f355aa28
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-09-25 14:16:19 +0200

    Added init dev server script

---
+++ scripts/init_dev_server.sh
@@ -0,0 +1,9 @@
+rm -f node_modules/ember-image-service
+rm -f node_modules/ember-components
+rm -f node_modules/ember-services
+rm -f node_modules/ember-core
+npm link ember-image-service
+npm link ember-components
+npm link ember-services
+npm link ember-core
+ember server
